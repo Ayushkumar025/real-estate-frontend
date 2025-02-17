c440ed7d86834dba2e2b54724b07851990deb8d8
Harsh Vardhan <harshvardhan232002@gmail.com>
2025-02-17 21:14:09 +0530

    Day's Last

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -5,15 +5,29 @@ import Footer from '../Footer/Footer'
 import PopularProperties from '../PopularProperties/PopularProperties'
 import GetAllProperties from '../GetAllProperties.jsx/GetAllProperties'
 import PostSellerProperty from '../PostProperty/PostProperty.jsx'
+import Carousel from '../Carousel/Carousel.jsx'
 
 const Home = () => {
+
+  const imageArray = [
+    "./Images/images.jpeg",
+    "./Images/1.jpg",
+    "./Images/2.jpg",
+    "./Images/3.jpg",
+    "./Images/4.jpeg"
+  ];
+
   return (
     <>
       <Header/>
       <SearchHouse/>
+      <div>
+        <div className="m-5 mb-20 flex justify-center items-center">
+          <Carousel images={imageArray} />
+        </div>
+      </div>
       <GetAllProperties/>
       <PostSellerProperty/>
-      {/* <PopularProperties/> */}
       <Footer/>
     </>
   )
